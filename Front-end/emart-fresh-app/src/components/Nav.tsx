9efc54b0c099e7a919f76b1eabe0d667dc70aa8f
jung jinseong <wlstjd545@gmail.com>
2023-09-22 10:23:06 +0900

    :sparkles: 도커파일 삽입
    프론트 엔드 nginx 도커파일

--- Front-end/emart-fresh-app/src/components/Nav.tsx
+++ Front-end/emart-fresh-app/src/components/Nav.tsx
@@ -1,17 +1,17 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import styles from "./comp_css/nav.module.css";
+import styles from "./comp_css/Nav.module.css";
 
 import { Link, useLocation, useNavigate } from "react-router-dom";
 
 import { useState } from "react";
-import Badge, { BadgeProps } from '@mui/material/Badge';
-import { styled } from '@mui/material/styles';
-import IconButton from '@mui/material/IconButton';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import { useRecoilState } from 'recoil';
+import Badge, { BadgeProps } from "@mui/material/Badge";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { useRecoilState } from "recoil";
 import { loginState } from "../atoms";
 
-export default function Nav() { 
+export default function Nav() {
   const navigate = useNavigate();
   const location = useLocation();
   const isHomePage = location.pathname === "/"; // 홈 페이지 여부 확인
@@ -19,18 +19,18 @@ export default function Nav() {
 
   const logout = () => {
     setLoginToken({
-      accessToken: '',
-      refreshToken: '',
+      accessToken: "",
+      refreshToken: "",
     });
-    alert('로그아웃 완료! (임시 알림)')
-  }
+    alert("로그아웃 완료! (임시 알림)");
+  };
 
   const StyledBadge = styled(Badge)<BadgeProps>(({ theme }) => ({
-    '& .MuiBadge-badge': {
+    "& .MuiBadge-badge": {
       right: -3,
       top: 13,
       border: `2px solid ${theme.palette.background.paper}`,
-      padding: '0 4px',
+      padding: "0 4px",
     },
   }));
 
@@ -39,18 +39,24 @@ export default function Nav() {
       <nav className={styles.subNav}>
         <div className={styles.subContentDiv}>
           <span>
-            {loginToken.refreshToken !== '' ? <Link to="/" onClick={logout}>Logout</Link> : <Link to="/login">Login</Link>}
+            {loginToken.refreshToken !== "" ? (
+              <Link to="/" onClick={logout}>
+                Logout
+              </Link>
+            ) : (
+              <Link to="/login">Login</Link>
+            )}
           </span>
           <span>
             <Link to="/show">show</Link>
           </span>
           <span>
             {/* <Link to="/search">Search</Link> */}
-              <IconButton aria-label="cart" onClick={() => navigate('/cart')}>
-                <StyledBadge badgeContent={4} color="secondary">
-                  <ShoppingCartIcon />
-                </StyledBadge>
-              </IconButton>
+            <IconButton aria-label="cart" onClick={() => navigate("/cart")}>
+              <StyledBadge badgeContent={4} color="secondary">
+                <ShoppingCartIcon />
+              </StyledBadge>
+            </IconButton>
           </span>
           <span>
             <Link to="/list">MyPage</Link>
@@ -78,7 +84,7 @@ export default function Nav() {
         </div>
         <div></div>
       </nav>
-      <hr/>
+      <hr />
     </>
   );
 }
