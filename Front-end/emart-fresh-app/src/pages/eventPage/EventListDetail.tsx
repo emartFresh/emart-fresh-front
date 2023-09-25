582d410a1a9226209ffe76d8f73bf6024c2a72a4
7memoria7 <tgongneongje@gmail.com>
2023-09-25 13:24:31 +0900

    :white_check_mark: EvnetListDetail route 추가
    evnetListDetail route 추가

---
+++ Front-end/emart-fresh-app/src/pages/eventPage/EventListDetail.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./EventListDetail.module.css";
+
+const EventListDetail = () => {
+  return (
+    <>
+      <div>
+        <div className={styles.eventWrapper}></div>
+      </div>
+    </>
+  );
+};
+
+export default EventListDetail;
