7a3e9c57ae553fb1b89d753d2737e095b79cbb4b
7memoria7 <tgongneongje@gmail.com>
2023-10-12 15:50:08 +0900

    :art: 메뉴바 auth값에 따른 내용 수정
    메뉴바 auth값에 따른 내용 수정

--- Front-end/emart-fresh-app/src/pages/myPageMenuBar/StoreManager.tsx
+++ Front-end/emart-fresh-app/src/pages/myPageMenuBar/StoreManager.tsx
@@ -6,6 +6,19 @@ export default function StoreManager() {
   return (
     <div>
       <div className={styles.mypagemenubar}>
+        <Link to="/mypageMain">개인정보관리</Link>
+      </div>
+      <div className={styles.mypagemenubar}>
+        <Link to="/mypageMain/myorder">나의주문내역</Link>
+      </div>
+      <div className={styles.mypagemenubar}>
+        <Link to="/mypageMain/myreview">내가작성한리뷰</Link>
+      </div>
+      <div className={styles.mypagemenubar}>
+        <Link to="/mypageMain/mycoupon">나의쿠폰조회</Link>
+      </div>
+
+      <div className={styles.mypagemenubar}>
         <Link to="/">상품등록</Link>
       </div>
       <div className={styles.mypagemenubar}>
