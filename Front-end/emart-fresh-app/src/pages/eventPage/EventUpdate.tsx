d3e958c25dde74c41e595f33c78edfe367b85deb
7memoria7 <tgongneongje@gmail.com>
2023-10-23 09:59:17 +0900

    :zap: 이미지 인코딩 추가 및 이벤트 중복 등록 방지
    이미지 인코딩 추가 및 이벤트 중복 등록 방지

--- Front-end/emart-fresh-app/src/pages/eventPage/EventUpdate.tsx
+++ Front-end/emart-fresh-app/src/pages/eventPage/EventUpdate.tsx
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useState, ChangeEvent, FormEvent, useRef, useEffect } from "react";
+import { useState, ChangeEvent, FormEvent, useRef } from "react";
 import styles from "./EventUpdate.module.css";
 import BannerImageIcon from "../../assets/images/BannerImageIcon.png";
 import DetailImageIcon from "../../assets/images/DetailImageIcon.png";
-import { SendLoginPageIfNotLogin } from "../../utils/LoginUtils";
-import icon from "../../assets/images/i-icon.webp";
-
-import { sendAxiosMediaPostRequest } from "../../utils/userUtils";
-import { loginState } from "../../atoms";
+import { webpImageIncoder } from "./webpImageIncoder";
 import { useRecoilState } from "recoil";
+import { loginState } from "../../atoms";
+import { sendAxiosMediaPostRequest } from "../../utils/userUtils";
 import { toast } from "react-toastify";
+import { SendLoginPageIfNotLogin } from "../../utils/LoginUtils";
 
 export default function EventUpdate() {
   SendLoginPageIfNotLogin();
@@ -29,21 +28,23 @@ export default function EventUpdate() {
   );
   const eventBannerImageInputRef = useRef<HTMLInputElement>(null);
   const eventDetailImageInputRef = useRef<HTMLInputElement>(null);
-  //
+
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
 
   const handleBannerImageFileClick = () => {
     if (eventBannerImageInputRef.current) {
       eventBannerImageInputRef.current.click();
     }
   };
+
   const handleDetailImageFileClick = () => {
     if (eventDetailImageInputRef.current) {
       eventDetailImageInputRef.current.click();
     }
   };
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value, files } = e.target;
     console.log("이름  파일 벨류", name, value, files);
     if (files && files.length > 0) {
@@ -52,23 +53,20 @@ export default function EventUpdate() {
         ...formData,
         [name]: selectedFile,
       });
+      const webpfile = await webpImageIncoder(selectedFile);
 
-      const reader = new FileReader();
-      reader.onloadend = () => {
-        const imagePreview = reader.result as string;
-        if (name === "eventBannerImage") {
-          setBannerImagePreview(imagePreview);
-        } else if (name === "eventDetailImage") {
-          setDetailImagePreview(imagePreview);
-        }
-      };
-      reader.readAsDataURL(selectedFile);
+      if (name === "eventBannerImage") {
+        setBannerImagePreview(webpfile + "");
+        console.log("첫번째 배너", webpfile);
+      } else if (name === "eventDetailImage") {
+        setDetailImagePreview(webpfile + "");
+        console.log("두번째 디테일", webpfile);
+      }
     } else {
       setFormData((formData) => ({
         ...formData,
         [name]: value,
       }));
-
       if (name === "eventBannerImage") {
         setBannerImagePreview(BannerImageIcon);
       } else if (name === "eventDetailImage") {
@@ -77,66 +75,72 @@ export default function EventUpdate() {
     }
   };
 
-  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(true);
-  useEffect(() => {
-    const isTitleValid = formData.eventTitle.trim() !== "";
-    const isDateValid =
-      formData.eventStartDate &&
-      formData.eventEndDate &&
-      new Date(formData.eventEndDate) >= new Date(formData.eventStartDate);
-    const isBannerImageValid = formData.eventBannerImage;
-    const isDetailImage = formData.eventDetailImage;
-
-    console.log(isTitleValid);
-    console.log(isDateValid);
-    console.log(isBannerImageValid);
-    console.log(isDetailImage);
-
-    setIsButtonDisabled(
-      !(isTitleValid && isDateValid && isBannerImageValid && isDetailImage)
-    );
-  }, [formData]);
-
-  // 폼제출
+  // 폼 제출
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-
+    console.log("Submit button clicked");
     if (isSubmitting) {
       return;
     }
     setIsSubmitting(true);
 
-    const formDataToSend = new FormData();
-    formDataToSend.append("eventTitle", formData.eventTitle);
-    formDataToSend.append("eventBannerImage", formData.eventBannerImage || "");
-    formDataToSend.append("eventDetailImage", formData.eventDetailImage || "");
-    formDataToSend.append("eventStartDate", formData.eventStartDate || "");
-    formDataToSend.append("eventEndDate", formData.eventEndDate || "");
+    if (!formData.eventTitle && formData.eventTitle.trim() === "") {
+      toast.error("이벤트 제목을 입력하세요");
+      setIsSubmitting(false);
+    } else if (!formData.eventStartDate) {
+      toast.error("이벤트 시작날짜를 입력하세요");
+      setIsSubmitting(false);
+    } else if (!formData.eventEndDate) {
+      toast.error("이벤트 종료날짜를 입력하세요");
+      setIsSubmitting(false);
+    } else if (
+      new Date(formData.eventEndDate) < new Date(formData.eventStartDate)
+    ) {
+      toast.error("이벤트 시작날짜는 종료날짜보다 빨라야합니다.");
+      setIsSubmitting(false);
+    } else if (!formData.eventBannerImage || !formData.eventDetailImage) {
+      toast.error("이벤트 이미지를 등록하세요");
+      setIsSubmitting(false);
+    } else {
+      const formDataToSend = new FormData();
+      formDataToSend.append("eventTitle", formData.eventTitle);
+      formDataToSend.append(
+        "eventBannerImage",
+        formData.eventBannerImage || ""
+      );
+      formDataToSend.append(
+        "eventDetailImage",
+        formData.eventDetailImage || ""
+      );
+      formDataToSend.append("eventStartDate", formData.eventStartDate || "");
+      formDataToSend.append("eventEndDate", formData.eventEndDate || "");
 
-    const url = `${import.meta.env.VITE_BACK_PORT}/event/event-update`;
+      const url = `${import.meta.env.VITE_BACK_PORT}/event/event-update`;
 
-    sendAxiosMediaPostRequest(url, loginToken, setLoginToken, formDataToSend)
-      .then((response) => {
-        console.log("파일전송 : " + response);
+      sendAxiosMediaPostRequest(url, loginToken, setLoginToken, formDataToSend)
+        .then((response) => {
+          console.log("파일전송 : " + response);
 
-        toast.success("이벤트가 등록되었습니다!");
-        setFormData({
-          eventTitle: "",
-          eventBannerImage: null,
-          eventDetailImage: null,
-          eventStartDate: null,
-          eventEndDate: null,
+          toast.success("이벤트가 등록되었습니다!");
+          setFormData({
+            eventTitle: "",
+            eventBannerImage: null,
+            eventDetailImage: null,
+            eventStartDate: null,
+            eventEndDate: null,
+          });
+          setBannerImagePreview(BannerImageIcon);
+          setDetailImagePreview(DetailImageIcon);
+        })
+        .catch((error) => {
+          toast.error("이벤트 생성에 실패하였습니다.", error);
+        })
+        .finally(() => {
+          // 이벤트 등록 완료 후 상태 재설정
+          setIsSubmitting(false);
+          setIsButtonDisabled(true);
         });
-        setBannerImagePreview(BannerImageIcon);
-        setDetailImagePreview(DetailImageIcon);
-      })
-      .catch((error) => {
-        toast.error("이벤트 생성에 실패하였습니다.", error);
-      })
-      .finally(() => {
-        // 이벤트 등록 완료 후 상태 재설정
-        setIsSubmitting(false);
-      });
+    }
   };
 
   return (
@@ -152,14 +156,6 @@ export default function EventUpdate() {
             onChange={handleFileChange}
             placeholder="이벤트이름을 등록하세요"
           />
-          &nbsp;&nbsp;
-          <div className={styles.iconContainer}>
-            <img src={icon} style={{ width: "20px" }} />
-            <div className={styles.hoverText}>
-              시작날짜는 종료날짜보다 빨라야합니다.
-              <br />
-            </div>
-          </div>
         </div>
         <div className={styles.eventDate}>
           <div>
@@ -173,7 +169,7 @@ export default function EventUpdate() {
           </div>
           &nbsp;&nbsp;&nbsp;&nbsp;
           <div>
-            이벤트 종료날짜&nbsp;&nbsp;
+            이벤트 마지막날짜&nbsp;&nbsp;
             <input
               type="date"
               name="eventEndDate"
@@ -182,6 +178,7 @@ export default function EventUpdate() {
             />
           </div>
         </div>
+
         <div>
           <div>
             <p className={styles.pTag}>배너이미지</p>
@@ -230,9 +227,9 @@ export default function EventUpdate() {
       <button
         onClick={handleSubmit}
         className={`${styles.eventSubmitBtn} ${
-          isButtonDisabled || (isSubmitting && styles.disabledButton)
+          isSubmitting && styles.disabledButton
         }`}
-        disabled={isButtonDisabled || isSubmitting}
+        disabled={isSubmitting}
       >
         {isSubmitting ? "이벤트 등록 중..." : "이벤트 등록"}
       </button>
